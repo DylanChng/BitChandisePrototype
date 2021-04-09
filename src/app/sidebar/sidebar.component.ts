603cc377fae14d109035e6be1be0be26a4bc2922
Karl Choo Jia Long <karlchoo37@gmail.com>
2021-04-09 21:00:49 +0800

    Update sidebar.component.ts

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'app/auth/auth.service';
 
 
 export interface RouteInfo {
@@ -35,8 +36,28 @@ export const ROUTES: RouteInfo[] = [
 })
 
 export class SidebarComponent implements OnInit {
+
+    constructor(private authService:AuthService) {}
+    user = this.authService.getCurrentUser();
+    //adminPath = ["/nodes","/blockchain","register"];
+    //manufacturerPath = ["/nodes","/blockchain","register"];
+
     public menuItems: any[];
     ngOnInit() {
-        this.menuItems = ROUTES.filter(menuItem => menuItem);
+        //this.menuItems = ROUTES.filter(menuItem => menuItem);
+        if(this.user.userType == "admin"){
+            this.menuItems = [
+                { path: '/nodes',         title: 'BitChandise Nodes',       icon:'nc-tv-2',       class: '' },
+                { path: '/blockchain',    title: 'Blockchain',              icon:'nc-app',        class: '' },
+                { path: '/register',      title: 'Manufacturers',           icon:'nc-single-02',  class: '' },
+            ]
+        }else if(this.user.userType == "manufacturer"){
+            this.menuItems = [
+                { path: '/addItem',           title: 'Add Item',                  icon:'nc-simple-add', class: '' },
+                { path: '/updateItem',        title: 'Update Item',               icon:'nc-refresh-69', class: '' },
+                { path: '/viewCreatedItems',  title: 'View All Items',            icon:'nc-app',        class: '' },
+              { path: '/tracking',             title: 'Track Item',                icon:'nc-globe-2',    class: '' },
+            ]
+        }
     }
-}
+}   
